8eb842032eca691fcca0c8e627624643364934b6
mahimaarora2208 <marora1@umd.edu>
2022-12-05 02:56:03 -0500

    cppcheck and lint && README

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
 # roomba_ros2
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+---
+# Obstacle Avoidance in ROS2
+
+### Overview
+
+This is a ROS Package that defines a basic obstacle_avoidance using Gazebo and Turtlebot3.
+- Use of all five logger levels: ```Fatal, Error, Warn, Info and Debug.```
+
+### Dependencies/ Assumptions
+- OS : Ubuntu 20.04 
+- ROS2 Distro : ROS2 Galactic
+- Package build type : ```ament_cmake ```
+- Package dependencies : ```rclcpp```, ```std_msgs``` 
+- ROS2 Galactic Installation : [link](https://docs.ros.org/en/galactic/Installation/Ubuntu-Install-Debians.html)
+
+## How to Run the ROS Package
+### Build Instructions
+```
+cd <your_ROS2_ws>/src
+git clone https://github.com/mahimaarora2208/roomba_ros2.git
+cd ..   
+rosdep install -i --from-path src --rosdistro galactic -y
+colcon build --packages-select roomba_ros2
+source . install/setup.bash
+source ~/<your ROS2 installation>/opt/ros/galactic/setup.bash
+```
+
+### Run Publisher
+To run the publisher node, open a new terminal and run:
+```
+cd <your_ROS2_ws>
+. install/setup.bash
+ros2 run roomba_ros2 obstacle_avoidance
+```
+
+### Launch File
+To launch nodes using launch file and pass parameters using command line:
+```
+cd <ROS2_ws>/
+. install/setup.bash
+ 
+
+```
+### ROS2 Bags
+To record rosbags in a custom folder:
+```
+ros2 bag record -o rosbag_files /chatter
+```
+To check topic information that is in the rosbag:
+```
+ros2 bag info rosbag_files
+```
+Lastly, to play back the data from rosbags:
+```
+ros2 bag play rosbag_files
+```
+
+The results for the listener displaying output based on rosbags is in the result folder. Please note, it works independent of publisher node.
+## Results
+The results after running the following commands are stored in the <your_package>/results folder.
+
+### rqt Console
+```
+ ros2 run rqt_console rqt_console
+
+```
+### cppcheck
+Run the following command from the root directory of your ROS package
+```
+cppcheck --enable=all --std=c++17 ./src/*.cpp --suppress=missingIncludeSystem --suppress=unmatchedSuppression --suppress=unusedFunction --suppress=missingInclude --suppress=useInitializationList > results/cppcheck.txt
+```
+### cpplint
+Run the following command from the root directory of your ROS package
+```
+ cpplint --filter=-build/c++11,+build/c++17,-build/namespaces,-build/include_order ./src/*.cpp > ./results/cpplint.txt
+```
+### Google Styling format
+Run the following command from the directory where the .cpp files are present(src in this case)
+```
+clang-format -style=Google -i your_file.cpp
+```
--- src/obstacle_avoidance.cpp
+++ src/obstacle_avoidance.cpp
@@ -1,6 +1,53 @@
+/************************************************************************************
+ * Apache License 2.0
+ * Copyright (c) 2021, Mahima Arora
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ ************************************************************************************/
+/**
+ *  @file    obstacle_avoidance.cpp
+ *  @author  Mahima Arora
+ *  @date    12/03/2022
+ *  @version 1.0
+ *
+ *  @brief Source file to implement a simple ROS publisher node and a service
+ *         server node
+ *
+ *  @section DESCRIPTION
+ *
+ *  Source file to implement a simple ROS2 Obstacle avoidance node publishing a
+ * custom message and facilitate change in message content upon a request
+ *
+ */
+
+#include <geometry_msgs/msg/twist.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
-#include <geometry_msgs/msg/twist.hpp>
 
 using std::placeholders::_1;
 using namespace std::chrono_literals;
@@ -14,36 +61,34 @@ typedef enum {
   TURN,
 } StateType;
 
+/**
+ * @brief RoomBa Class
+ *
+ */
 class RoomBa : public rclcpp::Node {
  public:
-  RoomBa() :
-    Node("walker"),
-    state_(STOP)
-  {
-    
+  RoomBa() : Node("walker"), state_(STOP) {
     // creates publisher to publish /cmd_vel topic
     auto pubTopicName = "cmd_vel";
-    publisher_ = this->create_publisher<TWIST> (pubTopicName, 10);
+    publisher_ = this->create_publisher<TWIST>(pubTopicName, 10);
 
     auto default_qos = rclcpp::QoS(rclcpp::SensorDataQoS());
     // creates subscriber to get /demo_cam/camera/depth_demo topic
     auto subTopicName = "/demo_cam/camera/depth_demo";
     auto subCallback = std::bind(&RoomBa::subscribe_callback, this, _1);
-    subscription_ = this->create_subscription<sensor_msgs::msg::Image>(subTopicName, default_qos, subCallback);
-    
-    // create a 10Hz timer for processing 
-    
+    subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+        subTopicName, default_qos, subCallback);
+
+    // create a 10Hz timer for processing
+
     auto processCallback = std::bind(&RoomBa::process_callback, this);
-    timer_ = this->create_wall_timer (100ms, processCallback);
+    timer_ = this->create_wall_timer(100ms, processCallback);
   }
 
  private:
-  void subscribe_callback(const IMAGE& msg) {
-    lastImg_ = msg;
-  }
+  void subscribe_callback(const IMAGE& msg) { lastImg_ = msg; }
 
-  void process_callback()
-  {
+  void process_callback() {
     // Do nothing until the first data read
     if (lastImg_.header.stamp.sec == 0) {
       return;
@@ -53,69 +98,73 @@ class RoomBa : public rclcpp::Node {
     auto message = TWIST();
     // state machine (Mealy -- output on transition)
     switch (state_) {
-    case FORWARD:
-      if (hasObstacle()) {      // check transition
-        state_ = STOP;
-        publisher_->publish (message);
-        RCLCPP_INFO_STREAM (this->get_logger(), "State = STOP");
-      } 
-      break;
-    case STOP:
-      if (hasObstacle()) {    // check transition
-        state_ = TURN;
-        message.angular.z = 0.1;
-        publisher_->publish (message);
-        RCLCPP_INFO_STREAM (this->get_logger(), "State = TURN");
-      } else {
-        state_ = FORWARD;
-        message.linear.x = -0.1;
-        publisher_->publish (message);
-        RCLCPP_INFO_STREAM (this->get_logger(), "State = FORWARD");
-      }
-      break;
-    case TURN:
-      if (! hasObstacle()) {    // check transition
-        state_ = FORWARD;
-        message.linear.x = -0.1;
-        publisher_->publish (message);
-        RCLCPP_INFO_STREAM (this->get_logger(), "State = FORWARD");
-      }
-      break;
+      case FORWARD:
+        if (hasObstacle()) {  // check transition
+          state_ = STOP;
+          publisher_->publish(message);
+          RCLCPP_INFO_STREAM(this->get_logger(), "State = STOP");
+        }
+        break;
+      case STOP:
+        if (hasObstacle()) {  // check transition
+          state_ = TURN;
+          message.angular.z = 0.1;
+          publisher_->publish(message);
+          RCLCPP_INFO_STREAM(this->get_logger(), "State = TURN");
+        } else {
+          state_ = FORWARD;
+          message.linear.x = 0.1;
+          publisher_->publish(message);
+          RCLCPP_INFO_STREAM(this->get_logger(), "State = FORWARD");
+        }
+        break;
+      case TURN:
+        if (!hasObstacle()) {  // check transition
+          state_ = FORWARD;
+          message.linear.x = 0.1;
+          publisher_->publish(message);
+          RCLCPP_INFO_STREAM(this->get_logger(), "State = FORWARD");
+        }
+        break;
     }
   }
 
+  /**
+   * @brief hasObstacle() function return true if obstacle detected
+   *
+   *
+   */
 
-  bool hasObstacle () {
-    unsigned char *dataPtr = lastImg_.data.data();
-    float* floatData = (float*) dataPtr;
+  bool hasObstacle() {
+    unsigned char* dataPtr = lastImg_.data.data();
+    float* floatData = (float*)dataPtr;
 
     int idx;
-    for (unsigned int row=0; row < lastImg_.height -40 ; row++)
-      for (unsigned int col=0; col < lastImg_.width; col++) {
+    for (unsigned int row = 0; row < lastImg_.height - 40; row++)
+      for (unsigned int col = 0; col < lastImg_.width; col++) {
         idx = (row * lastImg_.width) + col;
-        if (floatData[idx] < 0.2) {
-          RCLCPP_INFO (this->get_logger(), "row=%d, col=%d, floatData[idx] = %.2f",
-                       row, col, floatData[idx]);
+        if (floatData[idx] < 1.0) {
+          RCLCPP_INFO(this->get_logger(),
+                      "row=%d, col=%d, floatData[idx] = %.2f", row, col,
+                      floatData[idx]);
           return true;
         }
       }
-    
+
     return false;
   }
-
 
   ////////////////////////////////////////
   // member variables
   ////////////////////////////////////////
   rclcpp::Subscription<IMAGE>::SharedPtr subscription_;
-  rclcpp::Publisher<TWIST>::SharedPtr    publisher_;
-  rclcpp::TimerBase::SharedPtr           timer_;
-  IMAGE                                  lastImg_;
-  StateType                              state_;
+  rclcpp::Publisher<TWIST>::SharedPtr publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  IMAGE lastImg_;
+  StateType state_;
 };
 
-int main(int argc, char ** argv)
-{
+int main(int argc, char** argv) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<RoomBa>());
   rclcpp::shutdown();
